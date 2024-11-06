34cef4438e4d53b0315900d8aa595bd771d62112
Felix Erdmann <hello@felixerdmann.com>
2024-11-06 14:37:11 +0100

    feat: integrate app_links for deep linking functionality

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -6,4 +6,7 @@ String mapboxAccessToken = dotenv.get('MAPBOX_ACCESS_TOKEN', fallback: "");
 final Guid senseBoxServiceUUID =
     Guid.fromString('CF06A218-F68E-E0BE-AD04-8EBC1EB0BC84');
 
+final Guid deviceInfoServiceUUID =
+    Guid.fromString('CF06A218-F68E-E0BE-AD04-8EBC1EB0BC85');
+
 String sentryDsn = dotenv.get('SENTRY_DSN', fallback: "");
