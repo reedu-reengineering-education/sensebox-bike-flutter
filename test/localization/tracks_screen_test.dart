fed57ececd697f9c293a30983d299b8154641043
Maria Zadnepryanets <mariya.z@gmail.com>
2025-03-26 09:30:10 +0100

    test: revert all changes to track_screen_test

--- test/localization/tracks_screen_test.dart
+++ test/localization/tracks_screen_test.dart
@@ -1,44 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sensebox_bike/services/isar_service.dart';
 import 'package:sensebox_bike/ui/screens/tracks_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../mocks.dart';
-import '../test_helpers.dart';
 
 // Alternative Isar mocking solutions: 
 // https://github.com/isar/isar/issues/1459
 // https://github.com/isar/isar/issues/294
 // https://github.com/isar/isar/issues/1147 (with Mockito)
 void main() {
-  late MockIsarService mockIsarService;
-
-  setUp(() {
-    mockIsarService = MockIsarService();
-  });
-
-  Widget buildTestWidget(Locale locale) {
-    return createLocalizedTestApp(
-      locale: locale,
-      child: Provider<IsarService>(
-        create: (_) => mockIsarService, // Provide the mock
-        child: const TracksScreen(),
-      ),
-    );
-  }
   group('TracksScreen Widget', () {
+    late MockIsarService mockIsarService;
+
+    setUp(() {
+      mockIsarService = MockIsarService();
+    });
+    Widget createTestApp(Locale locale) {
+      return MaterialApp(
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: locale,
+        home: Provider<IsarService>(
+          create: (_) => mockIsarService, // Provide the mock
+          child: const TracksScreen(),
+        ),
+      );
+    }
+
     testWidgets("is translated in English", (WidgetTester tester) async {
-      await tester.pumpWidget(buildTestWidget(const Locale('en')));
+      await tester.pumpWidget(createTestApp(const Locale('en')));
       expect(find.text('Tracks'), findsOneWidget);
     });
 
     testWidgets("is translated in German", (WidgetTester tester) async {
-      await tester.pumpWidget(buildTestWidget(const Locale('de')));
+      await tester.pumpWidget(createTestApp(const Locale('de')));
       expect(find.text('Tracks'), findsOneWidget);
     });
 
     testWidgets("is translated in Portugese", (WidgetTester tester) async {
-      await tester.pumpWidget(buildTestWidget(const Locale('pt')));
+      await tester.pumpWidget(createTestApp(const Locale('pt')));
       expect(find.text('Trajetos'), findsOneWidget);
     });
   });
