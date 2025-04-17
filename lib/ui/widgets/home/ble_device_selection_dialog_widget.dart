7f9724636e8cddb6481e0123df0c377ed46a98c4
Maria Zadnepryanets <mariya.z@gmail.com>
2025-04-17 13:09:49 +0200

    feat: revisit box selection dialog

--- lib/ui/widgets/home/ble_device_selection_dialog_widget.dart
+++ lib/ui/widgets/home/ble_device_selection_dialog_widget.dart
@@ -2,6 +2,7 @@ import 'package:sensebox_bike/blocs/ble_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:sensebox_bike/ui/widgets/common/custom_spacer.dart';
 
 void showDeviceSelectionDialog(BuildContext context, BleBloc bleBloc) {
   bleBloc.startScanning();
@@ -32,12 +33,35 @@ void showDeviceSelectionDialog(BuildContext context, BleBloc bleBloc) {
                   itemCount: snapshot.data!.length,
                   itemBuilder: (context, index) {
                     final device = snapshot.data![index];
-                    return ListTile(
-                      title: Text(device.platformName),
-                      onTap: () {
-                        bleBloc.connectToDevice(device, context);
-                        Navigator.pop(context);
-                      },
+                    return Column(
+                      children: [
+                        CustomSpacer(), // Add spacing between tiles
+                        Container(
+                          width: MediaQuery.of(context).size.width *
+                              0.9, // Set width to 80% of the screen
+                          margin: const EdgeInsets.only(
+                              top: 16), // Add margin above each item
+                          child: ListTile(
+                            title: Text(device.platformName),
+                            trailing: const Icon(
+                                Icons.arrow_forward_ios), // Add an arrow icon
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .outline, // Add border color
+                                width: 1, // Border width
+                              ),
+                              borderRadius: BorderRadius.circular(
+                                  8), // Add rounded corners
+                            ),
+                            onTap: () {
+                              bleBloc.connectToDevice(device, context);
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                      ],
                     );
                   },
                 );
