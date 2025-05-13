e2369aff735a276a6a2f4f5ea032f37fdfa604de
Maria Zadnepryanets <mariya.z@gmail.com>
2025-05-13 12:24:38 +0200

    refactor: SettingsScreen

--- lib/ui/screens/settings_screen.dart
+++ lib/ui/screens/settings_screen.dart
@@ -22,101 +22,131 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          // Settings Section
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              AppLocalizations.of(context)!.settingsGeneral,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ),
-          StreamBuilder<bool>(
-            stream: settingsBloc.vibrateOnDisconnectStream,
-            initialData: settingsBloc.vibrateOnDisconnect,
-            builder: (context, snapshot) {
-              return ListTile(
-                leading: const Icon(Icons.vibration),
-                title: Text(
-                    AppLocalizations.of(context)!.settingsVibrateOnDisconnect),
-                trailing: Switch(
-                  value: snapshot.data ?? false,
-                  onChanged: (value) {
-                    settingsBloc.toggleVibrateOnDisconnect(value);
-                  },
-                ),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.admin_panel_settings),
-            title: Text(AppLocalizations.of(context)!.generalPrivacyZones),
-            onTap: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const ExclusionZonesScreen()),
-            ),
-            trailing: Badge.count(
-              count: settingsBloc.privacyZones.length,
-              backgroundColor: Theme.of(context).iconTheme.color,
-            ),
-          ),
-
-          // Other Section
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              AppLocalizations.of(context)!.settingsOther,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.info),
-            title: Text(AppLocalizations.of(context)!.settingsAbout),
-            subtitle: FutureBuilder(
-              future: PackageInfo.fromPlatform(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Text(
-                    AppLocalizations.of(context)!.settingsVersion(
-                      '${snapshot.data!.version}+${snapshot.data!.buildNumber}'));
-                } else {
-                  return Text(AppLocalizations.of(context)!.generalLoading);
-                }
-              },
-            ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.privacy_tip),
-            title: Text(AppLocalizations.of(context)!.settingsPrivacyPolicy),
-            onTap: () {
-              launchUrlFunction(Uri.parse(senseBoxBikePrivacyPolicyUrl));
-            },
-          ),
-          // Help Section
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              AppLocalizations.of(context)!.settingsContact,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.contact_mail),
-            title: Text(AppLocalizations.of(context)!.settingsEmail),
-            onTap: () {
-              launchUrlFunction(Uri.parse(
-                  'mailto:$contactEmail?subject=senseBox:bike%20App'));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.bug_report),
-            title: Text(AppLocalizations.of(context)!.settingsGithub),
-            onTap: () {
-              launchUrlFunction(Uri.parse(gitHubNewIssueUrl));
-            },
-          ),
+          _buildGeneralSettingsSection(context, settingsBloc),
+          _buildOtherSection(context),
+          _buildHelpSection(context),
         ],
       ),
+    );
+  }
+
+  // General Settings Section
+  Widget _buildGeneralSettingsSection(
+      BuildContext context, SettingsBloc settingsBloc) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildSectionHeader(
+            context, AppLocalizations.of(context)!.settingsGeneral),
+        StreamBuilder<bool>(
+          stream: settingsBloc.vibrateOnDisconnectStream,
+          initialData: settingsBloc.vibrateOnDisconnect,
+          builder: (context, snapshot) {
+            return ListTile(
+              leading: const Icon(Icons.vibration),
+              title: Text(
+                  AppLocalizations.of(context)!.settingsVibrateOnDisconnect),
+              trailing: Switch(
+                value: snapshot.data ?? false,
+                onChanged: (value) {
+                  settingsBloc.toggleVibrateOnDisconnect(value);
+                },
+              ),
+            );
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.admin_panel_settings),
+          title: Text(AppLocalizations.of(context)!.generalPrivacyZones),
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const ExclusionZonesScreen()),
+          ),
+          trailing: Badge.count(
+            count: settingsBloc.privacyZones.length,
+            backgroundColor: Theme.of(context).iconTheme.color,
+          ),
+        ),
+      ],
+    );
+  }
+
+  // Other Section
+  Widget _buildOtherSection(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildSectionHeader(
+            context, AppLocalizations.of(context)!.settingsOther),
+        ListTile(
+          leading: const Icon(Icons.info),
+          title: Text(AppLocalizations.of(context)!.settingsAbout),
+          subtitle: FutureBuilder(
+            future: PackageInfo.fromPlatform(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(AppLocalizations.of(context)!.settingsVersion(
+                    '${snapshot.data!.version}+${snapshot.data!.buildNumber}'));
+              } else {
+                return Text(AppLocalizations.of(context)!.generalLoading);
+              }
+            },
+          ),
+        ),
+        _buildUrlTile(
+          context,
+          icon: Icons.privacy_tip,
+          title: AppLocalizations.of(context)!.settingsPrivacyPolicy,
+          url: senseBoxBikePrivacyPolicyUrl,
+        ),
+      ],
+    );
+  }
+
+  // Help Section
+  Widget _buildHelpSection(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildSectionHeader(
+            context, AppLocalizations.of(context)!.settingsContact),
+        _buildUrlTile(
+          context,
+          icon: Icons.contact_mail,
+          title: AppLocalizations.of(context)!.settingsEmail,
+          url: 'mailto:$contactEmail?subject=senseBox:bike%20App',
+        ),
+        _buildUrlTile(
+          context,
+          icon: Icons.bug_report,
+          title: AppLocalizations.of(context)!.settingsGithub,
+          url: gitHubNewIssueUrl,
+        ),
+      ],
+    );
+  }
+
+  // Reusable Section Header
+  Widget _buildSectionHeader(BuildContext context, String title) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+    );
+  }
+
+  // Reusable URL Tile
+  Widget _buildUrlTile(BuildContext context,
+      {required IconData icon, required String title, required String url}) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      onTap: () {
+        launchUrlFunction(Uri.parse(url), mode: LaunchMode.externalApplication);
+      },
     );
   }
 }
