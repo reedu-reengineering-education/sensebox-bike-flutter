6d0711740f3dfdea7e5b28cfe12ca98d1c65ec7a
Maria Zadnepryanets <mariya.z@gmail.com>
2025-05-26 11:35:46 +0200

    fix: display mabox static preview for the routes that have only 1 point or don't move

--- lib/models/track_data.dart
+++ lib/models/track_data.dart
@@ -47,22 +47,33 @@ class TrackData {
     if (coordinates.isEmpty) {
       return "";
     }
-
-    if (coordinates.length == 1) {
+    // If there is only one location, or all locations are the same
+    if ((coordinates.length == 1) || (coordinates.toSet().length == 1)) {
       final singlePoint = coordinates.first;
+      // Add a tiny offset to the second point
+      // to ensure Mapbox can render it
+      final offset = 0.00005;
       final repeatedPoints = [
         [singlePoint.x, singlePoint.y],
-        [singlePoint.x, singlePoint.y]
+        [singlePoint.x, singlePoint.y + offset]
       ];
       return encodePolyline(repeatedPoints);
     }
-    // tolerance based on number of geolocations
+    // If there are fewer than 10 coordinates, no simplification is needed
+    if (coordinates.length < 10) {
+      final List<List<num>> castedCoordinates = geolocations
+          .map((geolocation) => [geolocation.latitude, geolocation.longitude])
+          .toList();
+
+      return encodePolyline(castedCoordinates);
+    }
+
     final tolerance = calculateTolerance(coordinates.length);
-    // Simplify the polyline with a tolerance (e.g., 0.0001)
+
     final simplifiedCoordinates = simplify<Point<double>>(
       coordinates,
-      tolerance: tolerance, // Adjust tolerance to control simplification
-      highestQuality: false, // Use high-quality simplification
+      tolerance: tolerance,
+      highestQuality: false, 
     );
     // Convert simplified points back to a list of lists for encoding
     final simplifiedList =
