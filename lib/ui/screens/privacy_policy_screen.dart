1de3891d32c08e608f9f73655194fdb33a4d77ae
Maria Zadnepryanets <mariya.z@gmail.com>
2025-06-10 09:37:53 +0200

    refactor: PrivacyPolicyScreen

--- lib/ui/screens/privacy_policy_screen.dart
+++ lib/ui/screens/privacy_policy_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // For translations
 import 'package:sensebox_bike/constants.dart';
+import 'package:sensebox_bike/theme.dart';
 import 'package:sensebox_bike/ui/screens/app_home.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -28,7 +29,10 @@ class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
   @override
   void initState() {
     super.initState();
+    _initializeWebView();
+  }
 
+  void _initializeWebView() {
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..addJavaScriptChannel(
@@ -44,32 +48,7 @@ class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
       ..setNavigationDelegate(
         NavigationDelegate(
           onPageFinished: (String url) async {
-            // Inject JavaScript to extract the desired HTML content
-            await _controller.runJavaScript('''
-              (function() {
-                // Extract content between the specified comments
-                const containerStart = document.body.innerHTML.indexOf('<!-- <div class="container-fluid"> -->');
-                const containerEnd = document.body.innerHTML.indexOf('<!-- </div> -->', containerStart);
-                if (containerStart !== -1 && containerEnd !== -1) {
-                  const extractedContent = document.body.innerHTML.substring(containerStart, containerEnd);
-                  document.body.innerHTML = extractedContent; 
-                }
-
-                // Add a scroll event listener to detect when scrolled to the bottom
-                const sendMessageToFlutter = function(message) {
-                  ScrollHandler.postMessage(message);
-                };
-
-                window.onscroll = function() {
-                  const scrollPosition = window.scrollY || document.documentElement.scrollTop;
-                  const maxScroll = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-                  if (scrollPosition >= maxScroll - 10) {
-                    sendMessageToFlutter('scrolledToBottom');
-                  }
-                };
-              })();
-            ''');
-            // Hide the loader after the content is processed
+            await _injectJavaScript();
             setState(() {
               _isLoading = false;
             });
@@ -79,54 +58,82 @@ class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
       ..loadRequest(Uri.parse(senseBoxBikePrivacyPolicyUrl));
   }
 
+  Future<void> _injectJavaScript() async {
+    await _controller.runJavaScript('''
+      (function() {
+        // On the website page content is placed inside the following containers
+        const containerStart = document.body.innerHTML.indexOf('<!-- <div class="container-fluid"> -->');
+        const containerEnd = document.body.innerHTML.indexOf('<!-- </div> -->', containerStart);
+        if (containerStart !== -1 && containerEnd !== -1) {
+          const extractedContent = document.body.innerHTML.substring(containerStart, containerEnd);
+          document.body.innerHTML = extractedContent; 
+        }
+
+        const sendMessageToFlutter = function(message) {
+          ScrollHandler.postMessage(message);
+        };
+
+        window.onscroll = function() {
+          const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+          const maxScroll = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+          if (scrollPosition >= maxScroll - 10) {
+            sendMessageToFlutter('scrolledToBottom');
+          }
+        };
+      })();
+    ''');
+  }
+
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(localizations.settingsPrivacyPolicy),
-      ),
+        appBar: AppBar(title: Text(localizations.settingsPrivacyPolicy)),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
-      child: Column(
-        children: [
-          if (_isLoading) Center(child: CircularProgressIndicator())
-          else Expanded(child: WebViewWidget(controller: _controller)),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              children: [
-                Row(
+          padding: const EdgeInsets.symmetric(
+              vertical: spacing * 2, horizontal: spacing),
+          child: Column(
+            children: [
+              if (_isLoading)
+                Center(child: CircularProgressIndicator())
+              else
+                Expanded(child: WebViewWidget(controller: _controller)),
+              Padding(
+                padding: const EdgeInsets.all(spacing),
+                child: Column(
                   children: [
-                    Checkbox(
-                      value: _isCheckboxChecked,
-                      onChanged: (value) {
-                        setState(() {
-                          _isCheckboxChecked = value ?? false;
-                        });
-                      },
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: _isCheckboxChecked,
+                          onChanged: (value) {
+                            setState(() {
+                              _isCheckboxChecked = value ?? false;
+                            });
+                          },
+                        ),
+                        Expanded(
+                          child: Text(localizations.privacyPolicyAccept),
+                        ),
+                      ],
                     ),
-                    Expanded(
-                      child: Text(localizations.privacyPolicyAccept),
+                    const SizedBox(height: spacing),
+                    FilledButton(
+                      onPressed: (_isCheckboxChecked && _hasScrolledToBottom)
+                          ? () async {
+                              await _saveAcceptanceDate();
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const AppHome()),
+                              );
+                            }
+                          : null,
+                      child: Text(localizations.generalProceed),
                     ),
                   ],
                 ),
-                const SizedBox(height: 12),
-                FilledButton(
-                  onPressed: (_isCheckboxChecked && _hasScrolledToBottom)
-                      ? () async {
-                          await _saveAcceptanceDate(); 
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(builder: (context) => const AppHome()),
-                          );
-                        }
-                      : null,
-                  child: Text(localizations.generalProceed),
-                ),
-              ],
-            ),
           ),
         ],
       ),
