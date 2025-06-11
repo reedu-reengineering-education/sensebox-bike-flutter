15f4ae24ea57bf7ac7bb8ea806df794514f21c7c
Maria Zadnepryanets <mariya.z@gmail.com>
2025-06-11 13:43:17 +0200

    test: PrivacyPolicyScreen

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/foundation.dart';
+
+import 'package:flutter/material.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 import 'package:sensebox_bike/blocs/opensensemap_bloc.dart';
 import 'package:sensebox_bike/blocs/track_bloc.dart';
 import 'package:sensebox_bike/models/sensor_data.dart';
@@ -43,3 +45,98 @@ class MockOpenSenseMapBloc extends OpenSenseMapBloc {
 
 class MockSensor extends Mock implements Sensor {}
 class FakeSensorData extends Fake implements SensorData {}
+
+// Mocks for webview_flutter
+
+class FakeWebViewPlatform extends WebViewPlatform {
+  @override
+  PlatformWebViewController createPlatformWebViewController(
+    PlatformWebViewControllerCreationParams params,
+  ) {
+    return FakeWebViewController(params);
+  }
+
+  @override
+  PlatformWebViewWidget createPlatformWebViewWidget(
+    PlatformWebViewWidgetCreationParams params,
+  ) {
+    return FakeWebViewWidget(params);
+  }
+
+  @override
+  PlatformWebViewCookieManager createPlatformCookieManager(
+    PlatformWebViewCookieManagerCreationParams params,
+  ) {
+    return FakeCookieManager(params);
+  }
+
+  @override
+  PlatformNavigationDelegate createPlatformNavigationDelegate(
+    PlatformNavigationDelegateCreationParams params,
+  ) {
+    return FakeNavigationDelegate(params);
+  }
+}
+
+class FakeWebViewController extends PlatformWebViewController {
+  FakeWebViewController(super.params) : super.implementation();
+
+  @override
+  Future<void> setJavaScriptMode(JavaScriptMode javaScriptMode) async {}
+
+  @override
+  Future<void> setBackgroundColor(Color color) async {}
+
+  @override
+  Future<void> setPlatformNavigationDelegate(
+    PlatformNavigationDelegate handler,
+  ) async {}
+
+  @override
+  Future<void> addJavaScriptChannel(
+      JavaScriptChannelParams javaScriptChannelParams) async {}
+
+  @override
+  Future<void> loadRequest(LoadRequestParams params) async {}
+
+  @override
+  Future<String?> currentUrl() async {
+    return 'https://www.google.com';
+  }
+}
+
+class FakeCookieManager extends PlatformWebViewCookieManager {
+  FakeCookieManager(super.params) : super.implementation();
+}
+
+class FakeWebViewWidget extends PlatformWebViewWidget {
+  FakeWebViewWidget(super.params) : super.implementation();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Fake WebView Content', key: Key('FakeWebViewWidget'));
+  }
+}
+
+class FakeNavigationDelegate extends PlatformNavigationDelegate {
+  FakeNavigationDelegate(super.params) : super.implementation();
+
+  @override
+  Future<void> setOnNavigationRequest(
+    NavigationRequestCallback onNavigationRequest,
+  ) async {}
+
+  @override
+  Future<void> setOnPageFinished(PageEventCallback onPageFinished) async {}
+
+  @override
+  Future<void> setOnPageStarted(PageEventCallback onPageStarted) async {}
+
+  @override
+  Future<void> setOnProgress(ProgressCallback onProgress) async {}
+
+  @override
+  Future<void> setOnWebResourceError(
+    WebResourceErrorCallback onWebResourceError,
+  ) async {}
+}
---
+++ test/ui/screens/privacy_policy_screen_test.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:sensebox_bike/constants.dart';
+import 'package:sensebox_bike/ui/screens/privacy_policy_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:webview_flutter/webview_flutter.dart';
+import '../../test_helpers.dart';
+import '../../mocks.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+void main() {
+  late MockHttpClient mockHttpClient;
+  const htmlContent = '''
+    <html>
+      <body>
+        <div class="container-fluid">Privacy Policy Content</div>
+      </body>
+    </html>
+  ''';
+
+  setUpAll(() {
+    WebViewPlatform.instance = FakeWebViewPlatform();
+  });
+
+  setUp(() {
+    initializeTestDependencies();
+    mockHttpClient = MockHttpClient();
+  });
+
+  group('PrivacyPolicyScreen Tests', () {
+    testWidgets('displays loading indicator while fetching privacy policy',
+        (WidgetTester tester) async {
+      when(() => mockHttpClient.get(Uri.parse(senseBoxBikePrivacyPolicyUrl)))
+          .thenAnswer((_) async => http.Response(htmlContent, 200));
+
+      await tester.pumpWidget(createLocalizedTestApp(
+        child: const PrivacyPolicyScreen(),
+        locale: const Locale('en'),
+      ));
+
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      // Verify the title of the page
+      expect(find.text('Privacy Policy'), findsOneWidget);
+
+      // Verify the hint text is displayed
+      expect(find.text('Please review and accept the Privacy Policy to continue using the app.'), findsOneWidget);
+
+      // Verify the checkbox is displayed and unchecked by default
+      final checkboxFinder = find.byType(Checkbox);
+      expect(checkboxFinder, findsOneWidget);
+      expect(tester.widget<Checkbox>(checkboxFinder).value, isFalse);
+
+      // Verify the button with text "Proceed" is displayed and disabled
+      final proceedButtonFinder = find.widgetWithText(FilledButton, 'Proceed');
+      expect(proceedButtonFinder, findsOneWidget);
+      expect(tester.widget<FilledButton>(proceedButtonFinder).enabled, isFalse);
+    });
+
+    // TBD: write more tests for the PrivacyPolicyScreen
+    // with inspiration from https://github.com/flutter/flutter/issues/117422
+  });
+}
