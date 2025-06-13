6a0d47afaba932c264c8188160b7156dfeee5336
Maria Zadnepryanets <mariya.z@gmail.com>
2025-06-13 12:15:31 +0200

    fix: failing tests

--- test/localization/tracks_screen_test.dart
+++ test/localization/tracks_screen_test.dart
@@ -3,76 +3,66 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:sensebox_bike/blocs/track_bloc.dart';
 import 'package:sensebox_bike/models/track_data.dart';
+import 'package:sensebox_bike/services/isar_service.dart';
+import 'package:sensebox_bike/services/isar_service/track_service.dart';
 import 'package:sensebox_bike/ui/screens/tracks_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import '../mocks.dart';
+
+class MockIsarService extends Mock implements IsarService {}
+
+class MockTrackService extends Mock implements TrackService {}
+
+class MockTrackBloc extends Mock implements TrackBloc {}
 
 void main() {
-  group('TracksScreen Widget', () {
-    late MockIsarService mockIsarService;
-    late MockTrackBloc mockTrackBloc;
+  late MockIsarService mockIsarService;
+  late MockTrackService mockTrackService;
+  late MockTrackBloc mockTrackBloc;
 
-    setUp(() {
-      mockIsarService = MockIsarService();
-      mockTrackBloc = MockTrackBloc();
-      when(() => mockTrackBloc.isarService).thenReturn(mockIsarService);
-      when(() => mockIsarService.trackService.getAllTracks())
-          .thenAnswer((_) async => []);
-    });
+  setUp(() {
+    mockIsarService = MockIsarService();
+    mockTrackService = MockTrackService();
+    mockTrackBloc = MockTrackBloc();
 
-    Future<void> pumpTracksScreen(WidgetTester tester, Locale locale) async {
-      final tracksFuture = Future.delayed(
-        const Duration(seconds: 1),
-        () => <TrackData>[],
-      );
-      when(() => mockIsarService.trackService.getAllTracks())
-          .thenAnswer((_) => tracksFuture);
+    when(() => mockIsarService.trackService).thenReturn(mockTrackService);
+    when(() => mockTrackBloc.isarService).thenReturn(mockIsarService);
+  });
 
-      await tester.pumpWidget(
-        MaterialApp(
-          localizationsDelegates: AppLocalizations.localizationsDelegates,
-          supportedLocales: AppLocalizations.supportedLocales,
-          locale: locale,
-          home: ChangeNotifierProvider<TrackBloc>.value(
-            value: mockTrackBloc,
-            child: const TracksScreen(),
-          ),
+  Future<void> pumpTracksScreen(WidgetTester tester, Locale locale) async {
+    when(() => mockTrackService.getAllTracks())
+        .thenAnswer((_) async => <TrackData>[]);
+
+    await tester.pumpWidget(
+      MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: locale,
+        home: ChangeNotifierProvider<TrackBloc>.value(
+          value: mockTrackBloc,
+          child: const TracksScreen(),
         ),
-      );
-    }
+      ),
+    );
 
+    await tester
+        .pumpAndSettle(); // Ensure all asynchronous operations are completed
+  }
+
+  group('TracksScreen Widget', () {
     testWidgets("is translated in English", (WidgetTester tester) async {
-      await tester.runAsync(() async {
-        await pumpTracksScreen(tester, const Locale('en'));
-
-        // Simulate the passage of time
-        await tester.pump(const Duration(seconds: 1));
-
-        expect(find.text('Tracks'), findsOneWidget);
-      });
+      await pumpTracksScreen(tester, const Locale('en'));
+      expect(find.text('Tracks'), findsOneWidget);
     });
 
     testWidgets("is translated in German", (WidgetTester tester) async {
-      await tester.runAsync(() async {
-        await pumpTracksScreen(tester, const Locale('de'));
-
-        // Simulate the passage of time
-        await tester.pump(const Duration(seconds: 1));
-
-        expect(find.text('Tracks'), findsOneWidget);
-      });
+      await pumpTracksScreen(tester, const Locale('de'));
+      expect(find.text('Tracks'), findsOneWidget);
     });
 
     testWidgets("is translated in Portuguese", (WidgetTester tester) async {
-      await tester.runAsync(() async {
-        await pumpTracksScreen(tester, const Locale('pt'));
-
-        // Simulate the passage of time
-        await tester.pump(const Duration(seconds: 1));
-
-        expect(find.text('Trajetos'), findsOneWidget);
-      });
+      await pumpTracksScreen(tester, const Locale('pt'));
+      expect(find.text('Trajetos'), findsOneWidget);
     });
   });
 }
--- test/ui/screens/tracks_screen_test.dart
+++ test/ui/screens/tracks_screen_test.dart
@@ -4,29 +4,37 @@ import 'package:mocktail/mocktail.dart';
 import 'package:provider/provider.dart';
 import 'package:sensebox_bike/blocs/track_bloc.dart';
 import 'package:sensebox_bike/models/track_data.dart';
+import 'package:sensebox_bike/services/isar_service.dart';
+import 'package:sensebox_bike/services/isar_service/track_service.dart';
 import 'package:sensebox_bike/ui/screens/tracks_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:sensebox_bike/ui/widgets/track/track_list_item.dart';
 
-import '../../mocks.dart';
+class MockIsarService extends Mock implements IsarService {}
+
+class MockTrackService extends Mock implements TrackService {}
+
+class MockTrackBloc extends Mock implements TrackBloc {}
 
 void main() {
   late MockIsarService mockIsarService;
+  late MockTrackService mockTrackService;
   late MockTrackBloc mockTrackBloc;
 
   setUp(() {
     mockIsarService = MockIsarService();
+    mockTrackService = MockTrackService();
     mockTrackBloc = MockTrackBloc();
+
+    when(() => mockIsarService.trackService).thenReturn(mockTrackService);
     when(() => mockTrackBloc.isarService).thenReturn(mockIsarService);
-    when(() => mockIsarService.trackService.getAllTracks())
-        .thenAnswer((_) async => []);
   });
 
   Future<void> pumpTracksScreen(
     WidgetTester tester, {
     required Future<List<TrackData>> tracksFuture,
   }) async {
-    when(() => mockIsarService.trackService.getAllTracks())
-        .thenAnswer((_) => tracksFuture);
+    when(() => mockTrackService.getAllTracks()).thenAnswer((_) => tracksFuture);
 
     await tester.pumpWidget(
       MaterialApp(
@@ -45,17 +53,21 @@ void main() {
   group('TracksScreen', () {
     testWidgets('displays loading indicator while tracks are loading',
         (WidgetTester tester) async {
-      await tester.runAsync(() async {
-        await pumpTracksScreen(
-          tester,
-          tracksFuture: Future.delayed(
-            const Duration(seconds: 1),
-            () => <TrackData>[],
-          ),
-        );
+      await pumpTracksScreen(
+        tester,
+        tracksFuture: Future.delayed(
+          const Duration(seconds: 1),
+          () => <TrackData>[],
+        ),
+      );
 
-        expect(find.byType(CircularProgressIndicator), findsOneWidget);
-      });
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+      await tester.pump(const Duration(seconds: 1));
+
+      expect(find.byType(TrackListItem), findsNothing);
     });
+
+    // Add more tests for pagination and other behaviors
   });
 }
