cf62b55516f0a5d31778abd2509d9ff4ebdced46
Maria Zadnepryanets <mariya.z@gmail.com>
2025-06-17 09:57:12 +0200

    chore: hide privacay policy consent behind feature flag

--- lib/ui/screens/initial_screen.dart
+++ lib/ui/screens/initial_screen.dart
@@ -3,6 +3,7 @@ import 'package:sensebox_bike/constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sensebox_bike/ui/screens/app_home.dart';
 import 'package:sensebox_bike/ui/screens/privacy_policy_screen.dart';
+import 'package:sensebox_bike/feature_flags.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
@@ -18,24 +19,27 @@ class _InitialScreenState extends State<InitialScreen> {
     _checkPrivacyPolicyAcceptance();
   }
 
-  Future<void> _checkPrivacyPolicyAcceptance() async {
+Future<void> _checkPrivacyPolicyAcceptance() async {
     final prefs = await SharedPreferences.getInstance();
-    final acceptedAt = prefs.getString(SharedPreferencesKeys.privacyPolicyAcceptedAt);
+    final acceptedAt =
+        prefs.getString(SharedPreferencesKeys.privacyPolicyAcceptedAt);
 
     if (!context.mounted) return;
 
-    if (acceptedAt != null) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const AppHome()),
-      );
-    } else {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const PrivacyPolicyScreen()),
-      );
+    final Widget nextScreen = _getNextScreen(acceptedAt);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => nextScreen),
+    );
+}
+
+Widget _getNextScreen(String? acceptedAt) {
+    if (!FeatureFlags.showPrivacyPolicyScreen) {
+      return const AppHome();
     }
-  }
+
+    return acceptedAt != null ? const AppHome() : const PrivacyPolicyScreen();
+}
 
   @override
   Widget build(BuildContext context) {
