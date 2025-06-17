a14caf49326d3ba6989f2dc74a0f7be93fe604be
Maria Zadnepryanets <mariya.z@gmail.com>
2025-06-17 13:39:10 +0200

    feat: refactor header on tracks screen

---
+++ lib/ui/screens/tracks_screen/tracks_screen_header.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:sensebox_bike/theme.dart';
+
+class TracksScreenHeader extends StatelessWidget {
+  final int trackCount;
+  final Duration totalDuration;
+  final double totalDistance;
+
+  const TracksScreenHeader({
+    required this.trackCount,
+    required this.totalDuration,
+    required this.totalDistance,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final localizations = AppLocalizations.of(context)!;
+
+    return SizedBox(
+      width: double.infinity,
+      height: 90,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(color: theme.colorScheme.surface),
+          ),
+          Positioned(
+            left: spacing,
+            top: spacing,
+            child: Container(
+              width: MediaQuery.of(context).size.width - 2 * spacing, // Responsive width
+              height: 71,
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(255, 255, 255, 0.18),
+                border: Border.all(color: theme.colorScheme.tertiary, width: borderWidth),
+                borderRadius: BorderRadius.circular(borderRadius),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(spacing),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _buildIconWithText(
+                      context,
+                      Icons.route,
+                      localizations.tracksAppBarSumTracks(trackCount),
+                    ),
+                    _buildIconWithText(
+                      context,
+                      Icons.timer_outlined,
+                      localizations.generalTrackDuration(
+                        totalDuration.inHours,
+                        totalDuration.inMinutes.remainder(60),
+                      ),
+                    ),
+                    _buildIconWithText(
+                      context,
+                      Icons.straighten,
+                      localizations.generalTrackDistance(
+                        totalDistance.toStringAsFixed(2),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIconWithText(BuildContext context, IconData icon, String text) {
+    final theme = Theme.of(context).textTheme;
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(icon, size: iconSizeLarge), 
+        const SizedBox(width: spacing / 2),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              text.split(' ')[0], 
+              style: theme.bodyLarge?.copyWith(
+                height: 1.0, // Remove extra line height
+              ),
+            ),
+            const SizedBox(width: spacing / 2), 
+            Text(
+              text.split(' ').sublist(1).join(' '), 
+              style: theme.bodySmall
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
