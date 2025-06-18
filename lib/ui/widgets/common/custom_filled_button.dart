a840d2952b424716574de0c6ab7d4ef9bd3cd1c9
Maria Zadnepryanets <mariya.z@gmail.com>
2025-06-18 14:25:59 +0200

    refactor: extract CustomFilledButton and reuse it

---
+++ lib/ui/widgets/common/custom_filled_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:sensebox_bike/theme.dart';
+
+class CustomFilledButton extends StatelessWidget {
+  final String label;
+  final IconData? icon;
+  final VoidCallback onPressed;
+
+  const CustomFilledButton({
+    required this.label,
+    this.icon,
+    required this.onPressed,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return FilledButton.icon(
+      onPressed: onPressed,
+      icon: icon != null ? Icon(icon) : const SizedBox.shrink(), 
+      label: Text(label),
+      style: FilledButton.styleFrom(
+        backgroundColor: theme.colorScheme.tertiary.withOpacity(0.6),
+        side: BorderSide(
+          color: theme.colorScheme.tertiary,
+          width: borderWidthRegular,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: theme.buttonBorderRadius, 
+        ),
+      ),
+    );
+  }
+}
