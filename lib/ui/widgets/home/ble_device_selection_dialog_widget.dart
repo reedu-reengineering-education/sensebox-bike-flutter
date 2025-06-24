f8086b18948cd730785ef641dbaa4a3ac1bcd218
Maria Zadnepryanets <mariya.z@gmail.com>
2025-06-24 13:44:27 +0200

    refactor: make device selection a poput

--- lib/ui/widgets/home/ble_device_selection_dialog_widget.dart
+++ lib/ui/widgets/home/ble_device_selection_dialog_widget.dart
@@ -4,7 +4,6 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:sensebox_bike/theme.dart';
 import 'package:sensebox_bike/ui/widgets/common/clickable_tile.dart';
-import 'package:sensebox_bike/ui/widgets/common/custom_spacer.dart';
 
 void showDeviceSelectionDialog(BuildContext context, BleBloc bleBloc) async {
   Object? scanError;
@@ -15,14 +14,27 @@ void showDeviceSelectionDialog(BuildContext context, BleBloc bleBloc) async {
     scanError = e;
   }
 
-  showModalBottomSheet<void>(
-    showDragHandle: true,
+  showDialog<void>(
     context: context,
-    isScrollControlled: true,
-    builder: (BuildContext modalContext) {
-      return DeviceSelectionSheet(
-          bleBloc: bleBloc, initialScanError: scanError);
-    },
+      builder: (context) => AlertDialog(
+              title: Text(AppLocalizations.of(context)!.bleDeviceSelectTitle),
+              contentPadding: EdgeInsets.zero,
+              content: SizedBox(
+                height: 400,
+                width: 350, // Optionally set a width
+                child: DeviceSelectionSheet(
+                  bleBloc: bleBloc,
+                  initialScanError: scanError,
+                ),
+              ),
+              actions: [
+                TextButton(
+                    child: Text(AppLocalizations.of(context)!.generalCancel),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      bleBloc.stopScanning();
+                    })
+              ])
   );
 }
 
@@ -44,106 +56,74 @@ class _DeviceSelectionSheetState extends State<DeviceSelectionSheet> {
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
-    final colorScheme = Theme.of(context).colorScheme;
     
     return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            localizations.bleDeviceSelectTitle,
-            style: Theme.of(context).textTheme.headlineMedium,
-            textAlign: TextAlign.center,
-          ),
-          const CustomSpacer(),
-          if (widget.initialScanError != null)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Center(
-                child: Text(
-                  "Error starting scan: ${widget.initialScanError.toString()}",
-                  style: TextStyle(color: Theme.of(context).colorScheme.error),
-                  textAlign: TextAlign.center,
+        padding: const EdgeInsets.only(
+            top: spacing * 4, bottom: spacing, left: spacing, right: spacing),
+        child: StreamBuilder<List<BluetoothDevice>>(
+          stream: widget.bleBloc.devicesListStream,
+          builder: (context, snapshot) {
+            final colorScheme = Theme.of(context).colorScheme;
+
+            if (snapshot.connectionState == ConnectionState.waiting &&
+                !snapshot.hasData) {
+              return Center(
+                        child: CircularProgressIndicator(
+                      color: colorScheme.primaryFixedDim));
+            }
+
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("Stream Error: ${snapshot.error.toString()}"),
+              );
+            }
+
+            final devices = snapshot.data;
+
+            if (devices == null || devices.isEmpty) {
+              return ValueListenableBuilder<bool>(
+                valueListenable: widget.bleBloc.isScanningNotifier,
+                builder: (context, isScanning, child) {
+                  final colorScheme = Theme.of(context).colorScheme;
+                  if (isScanning) {
+                    return Center(
+                              child: CircularProgressIndicator(
+                            color: colorScheme.primaryFixedDim));
+                  } else {
+                    // Not scanning, and no devices found.
+                    return Center(
+                      child: Text(localizations.noBleDevicesFound),
+                    );
+                  }
+                },
+              );
+            }
+
+            return ListView.separated(
+              separatorBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.symmetric(horizontal: spacing * 2),
+                child: Divider(
+                  height: 1,
+                  color: colorScheme.primaryFixedDim,
                 ),
               ),
-            )
-          else
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.4,
-              child: StreamBuilder<List<BluetoothDevice>>(
-                stream: widget.bleBloc.devicesListStream,
-                builder: (context, snapshot) {
-                  final colorScheme = Theme.of(context).colorScheme;
-                  
-                  if (snapshot.connectionState == ConnectionState.waiting &&
-                      !snapshot.hasData) {
-                    return Center(
-                        child: CircularProgressIndicator(
-                            color: colorScheme.primaryFixedDim));
-                  }
-
-                  if (snapshot.hasError) {
-                    return Center(
-                      child: Text("Stream Error: ${snapshot.error.toString()}"),
-                    );
-                  }
-
-                  final devices = snapshot.data;
-
-                  if (devices == null || devices.isEmpty) {
-                    return ValueListenableBuilder<bool>(
-                      valueListenable: widget.bleBloc.isScanningNotifier,
-                      builder: (context, isScanning, child) {
-                        final colorScheme = Theme.of(context).colorScheme;
-                        if (isScanning) {
-                          return Center(
-                              child: CircularProgressIndicator(
-                                  color: colorScheme.primaryFixedDim));
-                        } else {
-                          // Not scanning, and no devices found.
-                          return Center(
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text(localizations.noBleDevicesFound),
-                            ),
-                          );
-                        }
-                      },
-                    );
-                  }
-
-                  return ListView.separated(
-                    separatorBuilder: (context, index) => Padding(
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: spacing * 2),
-                      child: Divider(
-                        height: 1,
-                        color: colorScheme.primaryFixedDim,
-                      ),
-                    ),
-                    shrinkWrap: true,
-                    itemCount: devices.length,
-                    itemBuilder: (context, index) {
-                      final device = devices[index];
-                      final deviceName = device.platformName.isNotEmpty
-                          ? device.platformName
-                          : "(Unknown)";
-                      return ClickableTile(
-                        child: Text(deviceName),
-                        onTap: () {
-                          widget.bleBloc.connectToDevice(device, context);
-                          Navigator.pop(context);
-                        },
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-        ],
-      ),
+              itemCount: devices.length,
+              itemBuilder: (context, index) {
+                final device = devices[index];
+                final deviceName = device.platformName.isNotEmpty
+                    ? device.platformName
+                    : "(Unknown)";
+                return ClickableTile(
+                  child: Text(deviceName),
+                  onTap: () {
+                    widget.bleBloc.connectToDevice(device, context);
+                    Navigator.pop(context);
+                  },
+                );
+              },
+            );
+          },
+        )
     );
   }
 }
