8189581168fafe21b7941db96a77e89d3f6d9c98
Felix Erdmann <hello@felixerdmann.com>
2025-08-29 13:03:59 +0200

    fix: update l10n to make it shorter

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -63,7 +63,8 @@ import 'app_localizations_pt.dart';
 /// be consistent with the languages listed in the AppLocalizations.supportedLocales
 /// property.
 abstract class AppLocalizations {
-  AppLocalizations(String locale) : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+  AppLocalizations(String locale)
+      : localeName = intl.Intl.canonicalizedLocale(locale.toString());
 
   final String localeName;
 
@@ -71,7 +72,8 @@ abstract class AppLocalizations {
     return Localizations.of<AppLocalizations>(context, AppLocalizations);
   }
 
-  static const LocalizationsDelegate<AppLocalizations> delegate = _AppLocalizationsDelegate();
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
 
   /// A list of this localizations delegate along with the default localizations
   /// delegates.
@@ -83,7 +85,8 @@ abstract class AppLocalizations {
   /// Additional delegates can be added by appending to this list in
   /// MaterialApp. This list does not have to be used at all if a custom list
   /// of delegates is preferred or required.
-  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      <LocalizationsDelegate<dynamic>>[
     delegate,
     GlobalMaterialLocalizations.delegate,
     GlobalCupertinoLocalizations.delegate,
@@ -844,7 +847,7 @@ abstract class AppLocalizations {
   /// No description provided for @loginRequiredMessage.
   ///
   /// In en, this message translates to:
-  /// **'Please log in to upload your sensor data to openSenseMap'**
+  /// **'Please log in to share sensor data'**
   String get loginRequiredMessage;
 
   /// No description provided for @errorExportDirectoryAccess.
@@ -1010,7 +1013,8 @@ abstract class AppLocalizations {
   String get trackStatistics;
 }
 
-class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
   const _AppLocalizationsDelegate();
 
   @override
@@ -1019,26 +1023,27 @@ class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> 
   }
 
   @override
-  bool isSupported(Locale locale) => <String>['de', 'en', 'pt'].contains(locale.languageCode);
+  bool isSupported(Locale locale) =>
+      <String>['de', 'en', 'pt'].contains(locale.languageCode);
 
   @override
   bool shouldReload(_AppLocalizationsDelegate old) => false;
 }
 
 AppLocalizations lookupAppLocalizations(Locale locale) {
-
-
   // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
-    case 'de': return AppLocalizationsDe();
-    case 'en': return AppLocalizationsEn();
-    case 'pt': return AppLocalizationsPt();
+    case 'de':
+      return AppLocalizationsDe();
+    case 'en':
+      return AppLocalizationsEn();
+    case 'pt':
+      return AppLocalizationsPt();
   }
 
   throw FlutterError(
-    'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
-    'an issue with the localizations generation tool. Please file an issue '
-    'on GitHub with a reproducible sample app and the gen-l10n configuration '
-    'that was used.'
-  );
+      'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+      'an issue with the localizations generation tool. Please file an issue '
+      'on GitHub with a reproducible sample app and the gen-l10n configuration '
+      'that was used.');
 }
--- lib/l10n/app_localizations_pt.dart
+++ lib/l10n/app_localizations_pt.dart
@@ -113,7 +113,8 @@ class AppLocalizationsPt extends AppLocalizations {
   String get openSenseMapLogin => 'Entrar com openSenseMap';
 
   @override
-  String get openSenseMapLoginDescription => 'Faça login para compartilhar seus dados.';
+  String get openSenseMapLoginDescription =>
+      'Faça login para compartilhar seus dados.';
 
   @override
   String get openSenseMapLogout => 'Sair';
@@ -140,19 +141,23 @@ class AppLocalizationsPt extends AppLocalizations {
   String get openSenseMapRegisterName => 'Nome';
 
   @override
-  String get openSenseMapRegisterNameErrorEmpty => 'O nome não pode estar vazio';
+  String get openSenseMapRegisterNameErrorEmpty =>
+      'O nome não pode estar vazio';
 
   @override
   String get openSenseMapRegisterPasswordConfirm => 'Confirmar senha';
 
   @override
-  String get openSenseMapRegisterPasswordConfirmErrorEmpty => 'A confirmação da senha não pode estar vazia';
+  String get openSenseMapRegisterPasswordConfirmErrorEmpty =>
+      'A confirmação da senha não pode estar vazia';
 
   @override
-  String get openSenseMapRegisterPasswordErrorMismatch => 'As senhas não coincidem';
+  String get openSenseMapRegisterPasswordErrorMismatch =>
+      'As senhas não coincidem';
 
   @override
-  String get openSenseMapRegisterPasswordErrorCharacters => 'A senha deve conter pelo menos 8 caracteres';
+  String get openSenseMapRegisterPasswordErrorCharacters =>
+      'A senha deve conter pelo menos 8 caracteres';
 
   @override
   String get openSenseMapRegisterFailed => 'Falha no registro';
@@ -164,7 +169,8 @@ class AppLocalizationsPt extends AppLocalizations {
   String get openSenseMapRegisterAcceptTermsPrivacy => 'termos de privacidade';
 
   @override
-  String get openSenseMapRegisterAcceptTermsError => 'Você deve aceitar os termos de privacidade';
+  String get openSenseMapRegisterAcceptTermsError =>
+      'Você deve aceitar os termos de privacidade';
 
   @override
   String get connectionButtonConnect => 'Conectar';
@@ -188,7 +194,8 @@ class AppLocalizationsPt extends AppLocalizations {
   String get bleDeviceSelectTitle => 'Toque para conectar';
 
   @override
-  String get noBleDevicesFound => 'Nenhuma senseBox encontrada. Certifique-se de que sua senseBox está ligada, toque fora desta janela e tente novamente.';
+  String get noBleDevicesFound =>
+      'Nenhuma senseBox encontrada. Certifique-se de que sua senseBox está ligada, toque fora desta janela e tente novamente.';
 
   @override
   String get selectOrCreateBox => 'Selecionar ou criar senseBox';
@@ -209,19 +216,22 @@ class AppLocalizationsPt extends AppLocalizations {
   String get createBoxNameError => 'O nome deve ter entre 2 e 50 caracteres';
 
   @override
-  String get createBoxGeolocationCurrentPosition => 'Sua posição atual será usada';
+  String get createBoxGeolocationCurrentPosition =>
+      'Sua posição atual será usada';
 
   @override
   String get openSenseMapBoxSelectionNoBoxes => 'Nenhum senseBox disponível';
 
   @override
-  String get openSenseMapBoxSelectionCreateHint => 'Crie um usando o botão \'+\'';
+  String get openSenseMapBoxSelectionCreateHint =>
+      'Crie um usando o botão \'+\'';
 
   @override
   String get openSenseMapBoxSelectionUnnamedBox => 'senseBox sem nome';
 
   @override
-  String get openSenseMapBoxSelectionIncompatible => 'Não compatível com senseBox:bike';
+  String get openSenseMapBoxSelectionIncompatible =>
+      'Não compatível com senseBox:bike';
 
   @override
   String get settingsGeneral => 'Geral';
@@ -253,13 +263,16 @@ class AppLocalizationsPt extends AppLocalizations {
   String get settingsGithub => 'GitHub issue';
 
   @override
-  String get privacyZonesStart => 'Toque no mapa para começar a desenhar uma área. Toque na marca de seleção para finalizar.';
+  String get privacyZonesStart =>
+      'Toque no mapa para começar a desenhar uma área. Toque na marca de seleção para finalizar.';
 
   @override
-  String get privacyZonesDelete => 'Toque em uma área para excluí-la. Toque na marca de seleção para finalizar.';
+  String get privacyZonesDelete =>
+      'Toque em uma área para excluí-la. Toque na marca de seleção para finalizar.';
 
   @override
-  String get trackDetailsPermissionsError => 'Permissão negada para salvar o arquivo no armazenamento externo.';
+  String get trackDetailsPermissionsError =>
+      'Permissão negada para salvar o arquivo no armazenamento externo.';
 
   @override
   String get trackDetailsFileSaved => 'Arquivo CSV salvo na pasta Downloads.';
@@ -280,7 +293,8 @@ class AppLocalizationsPt extends AppLocalizations {
   String get trackDelete => 'Excluir Trajeto';
 
   @override
-  String get trackDeleteConfirmation => 'Tem certeza de que deseja excluir este trajeto?';
+  String get trackDeleteConfirmation =>
+      'Tem certeza de que deseja excluir este trajeto?';
 
   @override
   String get sensorTemperature => 'Temperatura';
@@ -391,34 +405,43 @@ class AppLocalizationsPt extends AppLocalizations {
   String get connectionButtonEnableBluetooth => 'Ativar Bluetooth';
 
   @override
-  String get errorNoLocationAccess => 'Os serviços de localização estão desativados ou o acesso é negado. Para gravar faixas, ative os serviços de localização e permita que a aplicação aceda à sua localização nas definições do telefone.';
+  String get errorNoLocationAccess =>
+      'Os serviços de localização estão desativados ou o acesso é negado. Para gravar faixas, ative os serviços de localização e permita que a aplicação aceda à sua localização nas definições do telefone.';
 
   @override
-  String get errorNoScanAccess => 'Para se conectar à SenseBox, permita que a aplicação procure dispositivos próximos nas definições do telemóvel.';
+  String get errorNoScanAccess =>
+      'Para se conectar à SenseBox, permita que a aplicação procure dispositivos próximos nas definições do telemóvel.';
 
   @override
-  String get errorNoSenseBoxSelected => 'Para permitir o upload de dados do sensor para a nuvem, faça login na sua conta openSenseMap e selecione a caixa.';
+  String get errorNoSenseBoxSelected =>
+      'Para permitir o upload de dados do sensor para a nuvem, faça login na sua conta openSenseMap e selecione a caixa.';
 
   @override
-  String get loginRequiredMessage => 'Por favor, faça login para enviar seus dados do sensor para o openSenseMap';
+  String get loginRequiredMessage => 'Login para enviar seus dados do sensor';
 
   @override
-  String get errorExportDirectoryAccess => 'Erro ao acessar o diretório de exportação. Por favor, verifique se o aplicativo tem permissão para acessar o armazenamento.';
+  String get errorExportDirectoryAccess =>
+      'Erro ao acessar o diretório de exportação. Por favor, verifique se o aplicativo tem permissão para acessar o armazenamento.';
 
   @override
-  String get errorLoginFailed => 'Falha no login. Por favor, verifique suas credenciais.';
+  String get errorLoginFailed =>
+      'Falha no login. Por favor, verifique suas credenciais.';
 
   @override
-  String get errorRegistrationFailed => 'Falha no registro. Por favor, verifique suas credenciais.';
+  String get errorRegistrationFailed =>
+      'Falha no registro. Por favor, verifique suas credenciais.';
 
   @override
-  String get errorBleConnectionFailed => 'A conexão com a senseBox foi perdida. Por favor, certifique-se de que o Bluetooth está ativado e a senseBox está ligada.';
+  String get errorBleConnectionFailed =>
+      'A conexão com a senseBox foi perdida. Por favor, certifique-se de que o Bluetooth está ativado e a senseBox está ligada.';
 
   @override
-  String get errorUploadFailed => 'Falha no upload de dados. Por favor, verifique sua conexão com a internet e tente novamente.';
+  String get errorUploadFailed =>
+      'Falha no upload de dados. Por favor, verifique sua conexão com a internet e tente novamente.';
 
   @override
-  String get errorPermanentAuthentication => 'Falha permanente na autenticação. Por favor, faça login novamente para continuar fazendo upload.';
+  String get errorPermanentAuthentication =>
+      'Falha permanente na autenticação. Por favor, faça login novamente para continuar fazendo upload.';
 
   @override
   String get selectCsvFormat => 'Selecionar formato CSV';
@@ -433,19 +456,23 @@ class AppLocalizationsPt extends AppLocalizations {
   String get settingsDeleteAllData => 'Excluir Todos os Dados';
 
   @override
-  String get settingsDeleteAllDataConfirmation => 'Tem certeza de que deseja excluir todos os dados? Esta ação é irreversível.';
+  String get settingsDeleteAllDataConfirmation =>
+      'Tem certeza de que deseja excluir todos os dados? Esta ação é irreversível.';
 
   @override
-  String get settingsDeleteAllDataSuccess => 'Todos os dados foram excluídos com sucesso.';
+  String get settingsDeleteAllDataSuccess =>
+      'Todos os dados foram excluídos com sucesso.';
 
   @override
-  String get settingsDeleteAllDataError => 'Falha ao excluir todos os dados. Por favor, tente novamente.';
+  String get settingsDeleteAllDataError =>
+      'Falha ao excluir todos os dados. Por favor, tente novamente.';
 
   @override
   String get accountManagement => 'Gerenciamento de Conta';
 
   @override
-  String get deleteAllHint => 'Isso excluirá todas as suas faixas do aplicativo.';
+  String get deleteAllHint =>
+      'Isso excluirá todas as suas faixas do aplicativo.';
 
   @override
   String get generalConfirmation => 'Confirmação';
@@ -454,7 +481,8 @@ class AppLocalizationsPt extends AppLocalizations {
   String get privacyPolicyAccept => 'Eu li e aceito a política de privacidade.';
 
   @override
-  String get trackNoGeolocations => 'Nenhuma geolocalização disponível para este trajeto.';
+  String get trackNoGeolocations =>
+      'Nenhuma geolocalização disponível para este trajeto.';
 
   @override
   String get tracksAppBarTitle => 'Seus trajetos';
