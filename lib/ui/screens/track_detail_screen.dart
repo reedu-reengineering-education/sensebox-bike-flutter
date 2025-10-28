9e10a9bec21a918c123cf592d9c9346dd655eb80
Maria Zadnepryanets <mariya.z@gmail.com>
2025-10-28 11:36:54 +0100

    refactor: extract gradient component and improve data representation

--- lib/ui/screens/track_detail_screen.dart
+++ lib/ui/screens/track_detail_screen.dart
@@ -24,6 +24,7 @@ import 'package:sensebox_bike/l10n/app_localizations.dart';
 import 'package:sensebox_bike/ui/widgets/track/sensor_tile_list.dart';
 import 'package:sensebox_bike/theme.dart';
 import 'package:intl/intl.dart';
+import 'package:sensebox_bike/ui/widgets/common/sensor_gradient_widget.dart';
 
 class TrackDetailScreen extends StatefulWidget {
   final TrackData track;
@@ -628,39 +629,10 @@ class _TrackDetailScreenState extends State<TrackDetailScreen> {
                 ),
               ),
             ),
-            Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: Column(children: [
-                  Container(
-                      height: 12,
-                      decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(20),
-                          ),
-                          gradient: LinearGradient(
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
-                            colors: _sensorType == 'distance'
-                                ? <Color>[
-                                    Colors.red,
-                                    Colors.orange,
-                                    Colors.green
-                                  ]
-                                : <Color>[
-                                    Colors.green,
-                                    Colors.orange,
-                                    Colors.red
-                                  ],
-                            tileMode: TileMode.mirror,
-                          ))),
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    Text(getMinSensorValue(_geolocations, _sensorType)
-                        .toStringAsFixed(1)),
-                    const Spacer(),
-                    Text(getMaxSensorValue(_geolocations, _sensorType)
-                        .toStringAsFixed(1))
-                  ])
-                ])),
+            SensorGradientWidget(
+              sensorType: _sensorType,
+              geolocations: _geolocations,
+            ),
             SensorTileList(
               sensorData: _sensorData,
               selectedSensorType: _sensorType,
