7b56db7c2e1fe142ba7f6590ac10033608560051
Maria Zadnepryanets <mariya.z@gmail.com>
2025-10-31 15:24:38 +0100

    fix: return UploadErrorClassifier

--- test/services/upload_retry_manager_test.dart
+++ test/services/upload_retry_manager_test.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/widgets.dart';
 import 'package:sensebox_bike/services/upload_retry_manager.dart';
-import 'package:sensebox_bike/services/direct_upload_service.dart';
+import 'package:sensebox_bike/services/upload_error_classifier.dart';
 import 'package:sensebox_bike/services/custom_exceptions.dart';
 
 void main() {
