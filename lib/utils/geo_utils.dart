2d1be1a448c148129e3decb4200f866a501080c6
Maria Zadnepryanets <mariya.z@gmail.com>
2025-12-02 21:25:33 +0100

    refactor: remove getDistance from geo_utils and delete file

--- lib/utils/geo_utils.dart
+++
@@ -1,8 +0,0 @@
-import 'package:sensebox_bike/models/geolocation_data.dart';
-import 'package:sensebox_bike/utils/distance_calculation_utils.dart';
-
-double getDistance(List<GeolocationData> geolocations) {
-  return calculateDistanceWithSimplify(geolocations);
-}
-
-
